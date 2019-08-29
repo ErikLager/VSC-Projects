6c9caaf6dc2b471a598845807f4de9434df43bb1
ErikLager <ErikLager@users.noreply.github.com>
2019-08-29 10:33:52 +0200

    Now it add the squares

--- JSLABB/Funktioner/Functions.js
+++ JSLABB/Funktioner/Functions.js
@@ -1,15 +1,28 @@
 
+//Summan av alla tal
 function sumFunktion (min, max){
-
 
 let sum = 0;
 
 for (let i=min;i <= max; i++){
-    sum += i;
+
+    sum += square(i);
+    
     }
 
 return sum; 
 }
 
-let summan = sumFunktion(1,2147483647);
+let summan = sumFunktion(1,5);
 console.log(summan);
+
+//Kvadrering 
+
+function square (tal){
+    let sqr = tal*tal;
+    return sqr;
+}
+
+let kvadrat = square(7);
+//console.log(kvadrat);
+
