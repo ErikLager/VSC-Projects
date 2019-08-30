dd9383f2d208188f5e088cf9d0a9c7e6e4653118
ErikLager <ErikLager@users.noreply.github.com>
2019-08-30 13:14:32 +0200

    Klickspel som visar din tid

--- JSLABB/html/events/events.js
+++ JSLABB/html/events/events.js
@@ -2,6 +2,7 @@ let myButton = document.getElementById("knapp");
 let i = 1;
 let meddelande = document.getElementById("message");
 let startTime = "";
+let lowScore = 10000000;
 
 function click_DOM(ev){
     let nr = Math.floor(Math.random()*500);
@@ -9,24 +10,24 @@ function click_DOM(ev){
     let myElement = document.getElementById("knapp");
     myElement.style.marginTop = nr + "px";
     myElement.style.marginLeft = nr2 + "px";
-    
-    
-           
-    
-
+    console.log(startTime);
     console.log(i);
     i++;
 
     if(i == 2){
         startTime = new Date().getTime();
     } 
-
     if (i == 11){
-        // console.log("KLAR!!!")
         let sluttime = new Date().getTime();
         let final = sluttime-startTime;
-        window.alert("GRATTIS!!! Du klarade det. Din tid var: " + final + " Millisekunder");
-
+        console.log(final/1000);
+        document.getElementById("message").innerHTML = ("Din tid: " + final/1000 + " sekunder.");
+        
+        if (final<lowScore){
+            lowScore = final;
+            console.log("Nytt rekord: "+lowScore/1000 + " Sekunder")
+        }
+        else {console.log("Din tid var inte lägre än rekordet")};
         
     }
     
