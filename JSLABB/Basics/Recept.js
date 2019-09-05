b397e7381754347d781e1e8fedd083f09519ea7d
ErikLager <ErikLager@users.noreply.github.com>
2019-09-05 10:54:08 +0200

    Recept V1.1 NOW WITH AMOUNT (xD)

--- JSLABB/Basics/Recept.js
+++ JSLABB/Basics/Recept.js
@@ -1,20 +1,39 @@
-let i = 0;
 let resept1 = {
     titel: "Rotfruk i ugn",
     rating: 4.5,
     
+    servings: 2.5,
     ingredience: [
-        "potatis",
-        "morot",
-        "palstenacka",
-        "kolrot",
-        "rödbetor",
-        "brokoli",
-        "blomkol",
-        "matolja",
-        "timjärn",
-        "salt",
-        "peppar",
+
+        {Namn: "potatis",
+         Mängd: 1},
+
+        {Namn: "morot",
+        Mängd: "1/2"},
+
+        {Namn: "plastenacka",
+        Mängd: "1/2"},
+
+        {Namn: "rödbetor",
+        Mängd: "1/2"},
+
+        {Namn: "brokoli",
+        Mängd: "2-3 bitar"},
+
+        {Namn: "blomkol",
+        Mängd: "2-3bitar"},
+
+        {Namn: "matolja",
+        Mängd: "skvett lite över"},
+
+        {Namn: "timjärn",
+        Mängd: "tillfredsställande xD"},
+
+        {Namn: "salt",
+        Mängd: "så det smakar gott"},
+
+        {Namn: "peppar",
+        Mängd: "så det blir småprickigt"},
         
     ],
     steps: [
@@ -23,10 +42,12 @@ let resept1 = {
         "häll på en skvätt ojla",
         "krydda till personliga mått",
         "Släng in i ugn på medel värde i 30 min",
-    ]
+    ],
 }
 console.log(resept1.titel);
 console.log("Betyg: " + resept1.rating + "/5");
+console.log("Portioner: "+resept1.servings);
+console.log("(Rekomenderas för "+resept1.servings+" personer)");
 console.log("Ingrediences: ");
 console.log("");
 for (i = 0; i< resept1.ingredience.length; i++){
