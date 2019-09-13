b79fe607911624cf6d245cbc9a8be7be1fd6d272
ErikLager <ErikLager@users.noreply.github.com>
2019-09-13 14:10:09 +0200

    Sida som går att kolla maten, nästan klar

--- JSLABB/Fetch/main.js
+++ JSLABB/Fetch/main.js
@@ -1,18 +1,43 @@
-fetch("https://jsonplaceholder.typicode.com/users")
+fetch("http://94.46.140.3:8080/teknikumMenu/api/menu")
     .then(Response => Response.json())
     
     .then(data => {
-        let users = "";
-        for (let userIndex = 0; userIndex < data.length; ++userIndex){
-            const user = data[userIndex]
+        console.log(data);
+        console.log(data.meals);
+        for (let matIndex = 0; matIndex < data.meals.length; ++matIndex){
+            const mat = data.meals [matIndex]            
+            let myNewMatElement = document.createElement("li");
+            myNewMatElement.innerHTML = `<b>${mat.day}</b>` + " " + mat.alt_dish;
+            document.getElementById("food").appendChild(myNewMatElement);
             
-            let myNewUserElement = document.createElement("li");
-            myNewUserElement.innerHTML = user.name;
-            document.getElementById("users").appendChild(myNewUserElement);
+            
+            
 
-            users +=user.name + " ";
-            console.log(users);
         }
+        for (let matIndex2 = 0; matIndex2 < data.meals.length; ++matIndex2){
+            const mat = data.meals [matIndex2]
+            let myNewMatElement = document.createElement("li");
+            myNewMatElement.innerHTML = `<b>${mat.day}</b>` + " " + mat.dish;
+            document.getElementById("alternativ").appendChild(myNewMatElement);
+            
+
+        }
+        
+
+        // let users = "";
+        // for (let userIndex = 0; userIndex < data.length; ++userIndex){
+        //     const user = data[userIndex]
+            
+        //     let myNewUserElement = document.createElement("li");
+        //     myNewUserElement.innerHTML = user.name;
+        //     document.getElementById("users").appendChild(myNewUserElement);
+
+        //     users +=user.name + " ";
+        //     console.log(users);
+        // }
         // document.body.innerHTML = users;
         
     })
+    document.getElementById("head");
+    let toDag = new Date();
+    toDag.getDay();
