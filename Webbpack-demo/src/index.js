963b4cc1c5abd1518fa29a646fc3fcf8f0f350bf
ErikLager <ErikLager@users.noreply.github.com>
2019-09-20 13:30:10 +0200

    3 fulla oktaver av omfång

--- Webbpack-demo/src/index.js
+++ Webbpack-demo/src/index.js
@@ -1,16 +1,224 @@
 import tone from "tone";
 var synth = new tone.Synth().toMaster();
-let myButton = document.getElementById("knapp");
-let minKnapp = document.getElementById("knapp2");
 
-myButton.onclick = click_DOM;
-function click_DOM(ev){
-    synth.triggerAttackRelease("D4", "4n");
-}
-minKnapp.onclick = click_DOME;
-function click_DOME(ev){
-    synth.triggerAttackRelease("Db4 ", "4n");
+let C0 = document.getElementById("C0");
+C0.onclick = click_DOM_C0;
+function click_DOM_C0(ev){
+    synth.triggerAttackRelease("C4", "8n");
 }
 
-myButton.onclick = click_DOM;
-minKnapp.onclick = click_DOME;
+let Ciss0 = document.getElementById("C#0");
+Ciss0.onclick = click_DOM_Ciss0;
+function click_DOM_Ciss0(ev){
+    synth.triggerAttackRelease("C#4", "8n");
+}
+
+let D0 = document.getElementById("D0");
+D0.onclick = click_DOM_D0;
+function click_DOM_D0(ev){
+    synth.triggerAttackRelease("D4", "8n");
+}
+
+let Diss0 = document.getElementById("D#0");
+Diss0.onclick = click_DOM_Diss0;
+function click_DOM_Diss0(ev){
+    synth.triggerAttackRelease("D#4", "8n");
+}
+
+let E0 = document.getElementById("E0");
+E0.onclick = click_DOM_E0;
+function click_DOM_E0(ev){
+    synth.triggerAttackRelease("E4", "8n");
+}
+
+let F0 = document.getElementById("F0");
+F0.onclick = click_DOM_F0;
+function click_DOM_F0(ev){
+    synth.triggerAttackRelease("F4", "8n");
+}
+
+let Fiss0 = document.getElementById("F#0");
+Fiss0.onclick = click_DOM_Fiss0;
+function click_DOM_Fiss0(ev){
+    synth.triggerAttackRelease("F#4", "8n");
+}
+
+let G0 = document.getElementById("G0");
+G0.onclick = click_DOM_G0;
+function click_DOM_G0(ev){
+    synth.triggerAttackRelease("G4", "8n");
+}
+
+let Giss0 = document.getElementById("G#0");
+Giss0.onclick = click_DOM_Giss0;
+function click_DOM_Giss0(ev){
+    synth.triggerAttackRelease("G#4", "8n");
+}
+
+let A0 = document.getElementById("A0");
+A0.onclick = click_DOM_A0;
+function click_DOM_A0(ev){
+    synth.triggerAttackRelease("A4", "8n");
+}
+
+let Aiss0 = document.getElementById("A#0");
+Aiss0.onclick = click_DOM_Aiss0;
+function click_DOM_Aiss0(ev){
+    synth.triggerAttackRelease("A#4", "8n");
+}
+
+let B0 = document.getElementById("B0");
+B0.onclick = click_DOM_B0;
+function click_DOM_B0 (ev){
+    synth.triggerAttackRelease("B4", "8n");
+}
+
+let C1 = document.getElementById("C1");
+C1.onclick = click_DOM_C1;
+function click_DOM_C1(ev){
+    synth.triggerAttackRelease("C5", "8n");
+}
+
+let Ciss1 = document.getElementById("C#1");
+Ciss1.onclick = click_DOM_Ciss1;
+function click_DOM_Ciss1(ev){
+    synth.triggerAttackRelease("C#5", "8n");
+}
+
+let D1 = document.getElementById("D1");
+D1.onclick = click_DOM_D1;
+function click_DOM_D1(ev){
+    synth.triggerAttackRelease("D5", "8n");
+}
+
+let Diss1 = document.getElementById("D#1");
+Diss1.onclick = click_DOM_Diss1;
+function click_DOM_Diss1(ev){
+    synth.triggerAttackRelease("D#5", "8n");
+}
+
+let E1 = document.getElementById("E1");
+E1.onclick = click_DOM_E1;
+function click_DOM_E1(ev){
+    synth.triggerAttackRelease("E5", "8n");
+}
+
+let F1 = document.getElementById("F1");
+F1.onclick = click_DOM_F1;
+function click_DOM_F1(ev){
+    synth.triggerAttackRelease("F5", "8n");
+}
+
+let Fiss1 = document.getElementById("F#1");
+Fiss1.onclick = click_DOM_Fiss1;
+function click_DOM_Fiss1(ev){
+    synth.triggerAttackRelease("F#5", "8n");
+}
+
+let G1 = document.getElementById("G1");
+G1.onclick = click_DOM_G1;
+function click_DOM_G1(ev){
+    synth.triggerAttackRelease("G5", "8n");
+}
+
+let Giss1 = document.getElementById("G#1");
+Giss1.onclick = click_DOM_Giss1;
+function click_DOM_Giss1(ev){
+    synth.triggerAttackRelease("G#5", "8n");
+}
+
+let A1 = document.getElementById("A1");
+A1.onclick = click_DOM_A1;
+function click_DOM_A1(ev){
+    synth.triggerAttackRelease("A5", "8n");
+}
+
+let Aiss1 = document.getElementById("A#1");
+Aiss1.onclick = click_DOM_Aiss1;
+function click_DOM_Aiss1(ev){
+    synth.triggerAttackRelease("A#5", "8n");
+}
+
+let B1 = document.getElementById("B1");
+B1.onclick = click_DOM_B1;
+function click_DOM_B1 (ev){
+    synth.triggerAttackRelease("B5", "8n");
+}
+
+let C2 = document.getElementById("C2");
+C2.onclick = click_DOM_C2;
+function click_DOM_C2(ev){
+    synth.triggerAttackRelease("C6", "8n");
+}
+
+let Ciss2 = document.getElementById("C#2");
+Ciss2.onclick = click_DOM_Ciss2;
+function click_DOM_Ciss2(ev){
+    synth.triggerAttackRelease("C#6", "8n");
+}
+
+let D2 = document.getElementById("D2");
+D2.onclick = click_DOM_D2;
+function click_DOM_D2(ev){
+    synth.triggerAttackRelease("D6", "8n");
+}
+
+let Diss2 = document.getElementById("D#2");
+Diss2.onclick = click_DOM_Diss2;
+function click_DOM_Diss2(ev){
+    synth.triggerAttackRelease("D#6", "8n");
+}
+
+let E2 = document.getElementById("E2");
+E2.onclick = click_DOM_E2;
+function click_DOM_E2(ev){
+    synth.triggerAttackRelease("E6", "8n");
+}
+
+let F2 = document.getElementById("F2");
+F2.onclick = click_DOM_F2;
+function click_DOM_F2(ev){
+    synth.triggerAttackRelease("F6", "8n");
+}
+
+let Fiss2 = document.getElementById("F#2");
+Fiss2.onclick = click_DOM_Fiss2;
+function click_DOM_Fiss2(ev){
+    synth.triggerAttackRelease("F#6", "8n");
+}
+
+let G2 = document.getElementById("G2");
+G2.onclick = click_DOM_G2;
+function click_DOM_G2(ev){
+    synth.triggerAttackRelease("G6", "8n");
+}
+
+let Giss2 = document.getElementById("G#2");
+Giss2.onclick = click_DOM_Giss2;
+function click_DOM_Giss2(ev){
+    synth.triggerAttackRelease("G#6", "8n");
+}
+
+let A2 = document.getElementById("A2");
+A2.onclick = click_DOM_A2;
+function click_DOM_A2(ev){
+    synth.triggerAttackRelease("A6", "8n");
+}
+
+let Aiss2 = document.getElementById("A#2");
+Aiss2.onclick = click_DOM_Aiss2;
+function click_DOM_Aiss2(ev){
+    synth.triggerAttackRelease("A#6", "8n");
+}
+
+let B2 = document.getElementById("B2");
+B2.onclick = click_DOM_B2;
+function click_DOM_B2 (ev){
+    synth.triggerAttackRelease("B6", "8n");
+}
+
+let C3 = document.getElementById("C3");
+C3.onclick = click_DOM_C3; 
+function click_DOM_C3 (ev){
+    synth.triggerAttackRelease("C7", "8n");
+}
